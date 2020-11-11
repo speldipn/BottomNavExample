f269a9ffbec8cf161abe1813b12b9f3424d5fcae
오준영 <ojun-yeong@ojun-yeong-ui-MacBookPro.local>
2020-11-11 18:56:07 +0900

    WIP

--- app/src/main/java/com/example/bottomnavexample/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavexample/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +18,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupViews() {
         var navHostFragment = supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
-        NavigationUI.setupWithNavController(bottomNavMenu, navHostFragment.navController)
+        bottomNavMenu.setupWithNavController(navHostFragment.navController)
 
         var appBarConfiguration = AppBarConfiguration(
             setOf(R.id.phoneFragment, R.id.shoppingFragment, R.id.settingsFragment, R.id.avatarFragment))
         setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
-
     }
 }
